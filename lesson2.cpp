9cdc5a73e188e8f873ffb040db7670c10d403035
Heineken <pummi79@yahoo.com>
2021-11-03 18:58:38 +0200

    initial release

---
+++ lesson2.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int a,b,c=1;
+    cout << "Enter number: ";
+    cin >> a;
+    cout << "Enter exponential: ";
+    cin >> b;
+	cout << "Result:"<< a << "^" << b ;
+    do {
+        c=c*a;
+        --b;
+    }
+    while (b>0);
+    cout << "=" << c; 
+    return 0;
+}
+	
